92011520656da6facb61594a34277887abdcd9fd
Djily SARR <djilysarr275@gmail.com>
2025-10-25 20:14:56 +0200

    update

--- quiz_platform/settings.py
+++ quiz_platform/settings.py
@@ -200,10 +200,7 @@ SIMPLE_JWT = {
 }
 
 # CORS Configuration
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:5173,http://127.0.0.1:5173').split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
